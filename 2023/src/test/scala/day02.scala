9a9797c24d5bff44c6b0ac87dd8e1a452a08c1a1
stefkn <stefan.nowak@tails.com>
2024-01-08 15:57:31 +0000

    update test cases for day2

--- 2023/src/test/scala/day02.scala
+++ 2023/src/test/scala/day02.scala
@@ -13,14 +13,28 @@ class Day2Suite extends munit.FunSuite {
     val filename = "data/day02/example.txt"
     val data = getFile(filename)
     val expected = 8
-    val obtained = day2(data)
+    val obtained = day2p1(data)
     assertEquals(obtained, expected)
   }
-  test("Real test case") {
+  test("Part 1") {
     val filename = "data/day02/day02.txt"
     val data = getFile(filename)
     val expected = 2406
-    val obtained = day2(data)
+    val obtained = day2p1(data)
+    assertEquals(obtained, expected)
+  }
+  test("Example test case part 2") {
+    val filename = "data/day02/example.txt"
+    val data = getFile(filename)
+    val expected = 2286
+    val obtained = day2p2(data)
+    assertEquals(obtained, expected)
+  }
+  test("Part 2") {
+    val filename = "data/day02/day02.txt"
+    val data = getFile(filename)
+    val expected = 78375
+    val obtained = day2p2(data)
     assertEquals(obtained, expected)
   }
 }
